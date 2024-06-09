eb418a11f6bc10564e44bce35f7cae6b6637955f
seungjae <0719imporvement@naver.com>
2024-06-09 23:23:07 +0900

    Add ScheduleInfo: 변수 추가

--- src/main/java/entity/SheduleInfo.java
+++ src/main/java/entity/SheduleInfo.java
@@ -1,4 +1,29 @@
 package entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Cleanup;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.sql.Date;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "scheduleinfo")
+@Table(name = "scheduleinfo")
 public class SheduleInfo {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int indexnumber;
+    private int schedulenumber;
+    private String subject;
+    private int day;
+    private String classroom;
+    @Column(name = "start_time")
+    private Date startTime;
+    @Column(name = "class_time")
+    private int classTime;
+    private int score;
 }
