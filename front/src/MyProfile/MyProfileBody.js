6265dec421b25c35dd8fa86a78573a6107ca03e5
junsung1202 <junsung101010@gmail.com>
2024-06-10 16:43:44 +0900

    css
    1

--- front/src/MyProfile/MyProfileBody.js
+++ front/src/MyProfile/MyProfileBody.js
@@ -1,10 +1,14 @@
 import React from "react";
+import PublicSidebar from "../UserPublic/PublicSidebar";
 
 function MyProfileBody(){
     return(
         <div>
-            <div className="">
+            <PublicSidebar/>
+            <div className="myprofile-container">
+                <div className="myprofile-img">
 
+                </div>
             </div>
         </div>
     );
